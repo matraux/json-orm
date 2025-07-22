f0c22354912413b90267a6c75ba14f707a2a826f
Daniel Žůrek <info@danielzurek.cz>
2025-07-22 18:05:46 +0200

    Refac: Entity memory usage optimization, removed readonly state.

--- src/Entity/Entity.php
+++ src/Entity/Entity.php
@@ -7,56 +7,64 @@ use JsonSerializable;
 use Matraux\JsonORM\Collection\Collection;
 use Matraux\JsonORM\Json\Property;
 use Matraux\JsonORM\Json\Reader;
+use Nette\Utils\Json;
+use Nette\Utils\JsonException;
 use Nette\Utils\Type;
 use ReflectionAttribute;
+use ReflectionClass;
 use ReflectionObject;
 use ReflectionProperty;
-use RuntimeException;
 use Stringable;
 
 abstract class Entity implements Stringable, JsonSerializable
 {
 
-	final protected function __construct(protected ?Reader $reader = null)
+	final protected function __construct()
 	{
-		if ($reader) {
-			$reflection = new ReflectionObject($this);
-			foreach ($reflection->getProperties() as $property) {
-				$propertyName = self::getProperty($property)->name ?? $property->getName();
+	}
 
-				if ($reader->offsetExists($propertyName)) {
-					if (!$type = Type::fromReflection($property)) {
-						continue;
+	final public static function create(): static
+	{
+		return new static();
+	}
+
+	final public static function fromReader(Reader $reader): static
+	{
+		$object = static::create();
+
+		$properties = new ReflectionClass(static::class)->getProperties();
+		foreach ($properties as $property) {
+			$propertyName = self::getProperty($property)->name ?? $property->getName();
+
+			if ($reader->offsetExists($propertyName)) {
+				if (!$type = Type::fromReflection($property)) {
+					continue;
+				}
+
+				$value = $reader[$propertyName];
+				$className = $type->isClass() && $type->isSimple() ? $type->getSingleName() : null;
+
+				if ($className) {
+					if (is_array($value) && is_subclass_of($className, self::class)) {
+						/** @var class-string<Entity> $className */
+						$entity = $className::fromReader($reader->withKey($propertyName));
+						$property->setValue($object, $entity);
+					} elseif (is_array($value) && is_subclass_of($className, Collection::class)) {
+						/** @var class-string<Collection<Entity>> $className */
+						$collection = $className::create($reader->withKey($propertyName));
+						$property->setValue($object, $collection);
+					} elseif ((is_string($value) || is_int($value)) && is_subclass_of($className, BackedEnum::class)) {
+						/** @var class-string<BackedEnum> $className */
+						$enum = $className::tryFrom($value);
+						$property->setValue($object, $enum);
 					}
-
-					$value = $reader[$propertyName];
-					$className = $type->isClass() && $type->isSimple() ? $type->getSingleName() : null;
-
-					if ($className) {
-						if (is_array($value) && is_subclass_of($className, self::class)) {
-							/** @var class-string<Entity> $className */
-							$entity = $className::create($reader->withKey($propertyName));
-							$property->setValue($this, $entity);
-						} elseif (is_array($value) && is_subclass_of($className, Collection::class)) {
-							/** @var class-string<Collection<Entity>> $className */
-							$collection = $className::create($reader->withKey($propertyName));
-							$property->setValue($this, $collection);
-						} elseif ((is_string($value) || is_int($value)) && is_subclass_of($className, BackedEnum::class)) {
-							/** @var class-string<BackedEnum> $className */
-							$enum = $className::tryFrom($value);
-							$property->setValue($this, $enum);
-						}
-					} elseif (is_scalar($value) || $value === null) {
-						$property->setValue($this, $value);
-					}
+				} elseif (is_scalar($value) || $value === null) {
+					$property->setValue($object, $value);
 				}
 			}
 		}
-	}
 
-	final public static function create(?Reader $reader = null): static
-	{
-		return new static($reader);
+		return $object;
 	}
 
 	/**
@@ -64,10 +72,8 @@ abstract class Entity implements Stringable, JsonSerializable
 	 */
 	final public function jsonSerialize(): array
 	{
-		$this->validateWriting();
-
 		$data = [];
-		$properties = (new ReflectionObject($this))->getProperties(ReflectionProperty::IS_PUBLIC);
+		$properties = new ReflectionObject($this)->getProperties(ReflectionProperty::IS_PUBLIC);
 		foreach ($properties as $property) {
 			if (!$property->isInitialized($this)) {
 				continue;
@@ -88,21 +94,11 @@ abstract class Entity implements Stringable, JsonSerializable
 	}
 
 	/**
-	 * @throws RuntimeException If Entity using Data
-	 */
-	final protected function validateWriting(): void
-	{
-		if ($this->reader) {
-			throw new RuntimeException(sprintf('"%s" is in read only state, because using "%s".', static::class, Reader::class));
-		}
-	}
-
-	/**
-	 * @throws RuntimeException If entity can not be serialized to JSON
+	 * @throws JsonException
 	 */
 	final public function __toString(): string
 	{
-		return json_encode($this) ?: throw new RuntimeException(sprintf('Unable JSON serialize "%s".', static::class));
+		return Json::encode($this);
 	}
 
 }
