0eccd4db6cd3d45e978b72f49dc0014640ea2a87
Daniel Žůrek <info@danielzurek.cz>
2025-07-23 21:12:04 +0200

    Fix: Removed test code

--- tests/index.php
+++ tests/index.php
@@ -2,23 +2,10 @@
 
 namespace Matraux\JsonORMTest;
 
-use Matraux\JsonORM\Json\JsonReader;
-use Matraux\JsonORMTest\Collection\CommonCollection;
-
 require_once __DIR__ . '/Bootstrap.php';
 
 Bootstrap::dumper();
 dump('Test dumper');
-
-$reader = JsonReader::fromFile(Bootstrap::Assets . 'general.json');
-
-foreach (CommonCollection::create($reader) as $entity) {
-	echo($entity->name) . '<br>';
-
-	foreach ($entity->items ?? [] as $item) {
-		echo($item->name) . '<br>';
-	}
-}
 
 dump('Exit dumper');
 exit;
