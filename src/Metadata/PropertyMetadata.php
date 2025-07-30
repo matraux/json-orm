ada3c70da412da849a3c8882beb35b78f14d684f
Daniel Žůrek <info@danielzurek.cz>
2025-07-31 00:43:35 +0200

    Refac: Readonly PropertyMetadata

--- src/Metadata/PropertyMetadata.php
+++ src/Metadata/PropertyMetadata.php
@@ -2,23 +2,26 @@
 
 namespace Matraux\JsonORM\Metadata;
 
-use Matraux\JsonORM\Exception\PropertyMetadataException;
 use Matraux\JsonORM\Json\JsonProperty;
 use Nette\Utils\Type;
 use ReflectionAttribute;
 use ReflectionProperty;
 
-final class PropertyMetadata
+final readonly class PropertyMetadata
 {
 
-	public readonly string $name;
+	public string $name;
 
-	public readonly ?string $className;
+	public string $index;
+
+	public ?string $className;
 
 	protected function __construct(protected readonly ReflectionProperty $reflection)
 	{
+		$this->name = $this->reflection->name;
+
 		$attributes = $this->reflection->getAttributes(JsonProperty::class, ReflectionAttribute::IS_INSTANCEOF);
-		$this->name = array_shift($attributes)?->newInstance()->name ?? $this->reflection->name;
+		$this->index = array_shift($attributes)?->newInstance()->name ?? $this->reflection->name;
 
 		$type = Type::fromReflection($this->reflection);
 		$this->className = $type?->isClass() ? $type->getSingleName() : null;
@@ -27,20 +30,6 @@ final class PropertyMetadata
 	public static function create(ReflectionProperty $reflection): static
 	{
 		return new static($reflection);
-	}
-
-	public function setValue(object $object, mixed $value): void
-	{
-		if ($object::class !== $this->reflection->class) {
-			throw new PropertyMetadataException(sprintf(
-				'Cannot set property $%s: expected %s, %s given.',
-				$this->reflection->name,
-				$this->reflection->class,
-				$object::class
-			));
-		}
-
-		$this->reflection->setValue($object, $value);
 	}
 
 }
