6f328c7294b856dbe4dc101080f1edfb49caf569
Daniel Žůrek <info@danielzurek.cz>
2025-07-31 02:35:13 +0200

    Feat: Interface JsonExplorer

---
+++ src/Json/JsonExplorer.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types = 1);
+
+namespace Matraux\JsonORM\Json;
+
+use ArrayAccess;
+use Countable;
+use IteratorAggregate;
+use Matraux\JsonORM\Exception\ReadonlyAccessException;
+use Stringable;
+
+/**
+ * @implements ArrayAccess<int|string,mixed>
+ * @implements IteratorAggregate<int|string,mixed>
+ */
+abstract class JsonExplorer implements ArrayAccess, IteratorAggregate, Countable, Stringable
+{
+
+	abstract public function withIndex(string|int $index): static;
+
+	/**
+	 * @throws ReadonlyAccessException
+	 */
+	final public function offsetSet(mixed $offset, mixed $value): void
+	{
+		throw new ReadonlyAccessException('Explorer is readonly');
+	}
+
+	/**
+	 * @throws ReadonlyAccessException
+	 */
+	final public function offsetUnset(mixed $offset): void
+	{
+		throw new ReadonlyAccessException('Explorer is readonly');
+	}
+
+}
