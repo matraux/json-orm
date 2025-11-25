41a5f20a94a4ab16ca75df885da5a8e0992c0273
Daniel Žůrek <info@danielzurek.cz>
2025-11-25 15:52:51 +0100

    Fix: tests index

--- tests/index.php
+++ tests/index.php
@@ -2,8 +2,6 @@
 
 namespace Matraux\JsonOrmTest;
 
-use Matraux\JsonOrm\Json\SimpleExplorer;
-use Matraux\JsonOrmTest\Dto\Collection\CommonCollection;
 use Matraux\JsonOrmTest\Utils\Tracy;
 use Tracy\Debugger;
 
@@ -11,11 +9,6 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 Tracy::setup();
 Debugger::dump('Start dump');
-
-foreach (CommonCollection::fromExplorer(SimpleExplorer::fromFile(__DIR__ . '/data/general.json')) as $commonEntity) {
-	bdump($commonEntity);
-	break;
-}
 
 Debugger::dump('Finish dump');
 exit;
