b1fd8d31307e2065375b20cb72256f5055b2ef0f
Daniel Žůrek <info@danielzurek.cz>
2026-03-10 15:57:45 +0100

    Refac: Codec interface

--- src/Codec/Codec.php
+++ src/Codec/Codec.php
@@ -3,13 +3,18 @@
 namespace Matraux\JsonOrm\Codec;
 
 use Matraux\JsonOrm\Json\Explorer;
-use Matraux\JsonOrm\Metadata\PropertyMetadata;
+use Matraux\JsonOrm\Metadata\Metadata;
 
+/**
+ * Implementing classes must also declare:
+ * #[\Attribute(\Attribute::TARGET_PROPERTY)]
+ * Otherwise the codec cannot be used as a property attribute.
+ */
 interface Codec
 {
 
-	public function encode(mixed $value, PropertyMetadata $property): mixed;
+	public function encode(mixed $value, Metadata $metadata): mixed;
 
-	public function decode(Explorer $explorer, PropertyMetadata $property): mixed;
+	public function decode(Explorer $explorer, Metadata $metadata): mixed;
 
 }
