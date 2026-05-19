66111e902638f3b06f642365765793416a5c9132
Daniel Žůrek <info@danielzurek.cz>
2026-05-19 17:52:30 +0200

    Fix: phpstan return type

--- src/Collection/Collection.php
+++ src/Collection/Collection.php
@@ -34,7 +34,7 @@ abstract class Collection implements Countable, ArrayAccess, JsonSerializable, S
 	final public static function create(): static
 	{
 		/** @var static<TEntity> */
-		return new static();
+		return new static(); //@phpstan-ignore varTag.nativeType
 	}
 
 	/**
@@ -43,7 +43,7 @@ abstract class Collection implements Countable, ArrayAccess, JsonSerializable, S
 	final public static function fromExplorer(Explorer $explorer): static
 	{
 		/** @var static<TEntity> */
-		return new static($explorer);
+		return new static($explorer); //@phpstan-ignore varTag.nativeType
 	}
 
 	final public function count(): int
